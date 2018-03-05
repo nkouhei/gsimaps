67b140a970dc154f1d958d13a2c02829fd1d3034
nkouhei <37054471+nkouhei@users.noreply.github.com>
2018-03-05 11:53:01 +0900

    Update gsimaps.js

--- js/gsimaps.js
+++ js/gsimaps.js
@@ -28,7 +28,7 @@ CONFIG.layersTab = [
 			'./layers_txt/layers_topic.txt',
 			'./layers_txt/layers_skhb.txt',
 			'./layers_txt/layers_topic_kusatsushirane.txt',
-			'./layers_txt/レイヤー.txt'
+			'./layers_txt/LAI.txt'
 					]
 	},
 	{
